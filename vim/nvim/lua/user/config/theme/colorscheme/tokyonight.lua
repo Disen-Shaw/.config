a2d08f32f4a0958cb754d5d441f50ed1f59e8d03
Disen Shaw <shaodisheng1314@gmail.com>
2022-10-16 23:34:28 +0800

    nvim-update

--- vim/nvim/lua/user/config/theme/colorscheme/tokyonight.lua
+++ vim/nvim/lua/user/config/theme/colorscheme/tokyonight.lua
@@ -1,24 +1,37 @@
-local function tokyonight()
+require("tokyonight").setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
+  transparent = true, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "transparent", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
-  vim.g.tokyonight_style = 'night'
+  --- You can override specific color groups to use other groups or a hex color
+  --- function will be called with a ColorScheme table
+  ---@param colors ColorScheme
+  on_colors = function(colors) end,
 
-  vim.g.tokyonight_terminal_colors = true             -- Config the color
-  vim.g.tokyonight_lualine_bold = true                -- Lualine section headers bold
+  --- You can override specific highlights to use other groups or a hex color
+  --- function will be called with a Highlights and ColorScheme table
+  ---@param highlights Highlights
+  ---@param colors ColorScheme
+  on_highlights = function(highlights, colors) end,
+})
 
-  vim.g.tokyonight_italic_keywords = false            -- keyworld Italic
-  vim.g.tokyonight_italic_comments = true             -- Comment Italic
-  vim.g.tokyonight_italic_variables = true            -- Variable Italic
-  vim.g.tokyonight_italic_functions = true            -- Function Italic
-
-  vim.g.tokyonight_hide_inactive_statusline = false
-  vim.g.tokyonight_transparent = true                 -- if transparent
-  vim.g.tokyonight_transparent_sidebar = true
-  vim.g.tokyonight_dark_sidebar = false               -- sidebar daker
-  vim.g.tokyonight_dark_float = false                 -- float window daker
-  vim.g.tokyonight_day_brightness = 0.3
-  -- vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-
-  vim.cmd[[colorscheme tokyonight]]
-end
-
-tokyonight()
+vim.cmd[[colorscheme tokyonight]]
