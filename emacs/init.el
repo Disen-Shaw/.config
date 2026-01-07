0afbd3bafe533d0a70d595966b1396157b71dc49
Disen-Shaw <disenshaw@gmail.com>
2026-01-07 20:58:23 +0800

    refactor: emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -4,19 +4,3 @@
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 (message ">>> Emacs config loadedd successfully <<<")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
- '(package-vc-selected-packages
-   '((tokyonight-themes :url
-			"https://github.com/xuchengpeng/tokyonight-themes"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
